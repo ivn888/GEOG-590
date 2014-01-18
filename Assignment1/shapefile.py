901c4e0cf539bd55b87c53727e36c243610b5727
Jason Roebuck <geojson@me.com>
2014-01-17 18:13:09 -0800

    shapefile assignment framework

---
+++ Assignment1/shapefile.py
@@ -0,0 +1,13 @@
+# 2014-01 Jason Roebuck
+# Product of work for GEOG 590 @ Portland State University
+# May be used for whatever!
+# github.com/jtroe/GEOG-590 - Fork me on github!
+import arcpy
+shapefile = r'states_21basic\states.shp'
+
+def main():
+    desc = arcpy.Describe(shapefile)
+    
+
+if __name__ == "__main__":
+    main()
