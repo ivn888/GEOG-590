e582f398fe6073d19be157f2bfc7d7eccb5f1fc5
Jason Roebuck <geojson@me.com>
2014-01-17 19:15:35 -0800

    add some comments

--- Assignment1/shapefile.py
+++ Assignment1/shapefile.py
@@ -5,31 +5,30 @@
 import arcpy
 import os
 
-shapefile = r'states_21basic\states.shp'
-
 def main():
     for shp in AllShapefiles():
         shpDesc = arcpy.Describe(shp)
-        print shpDesc.name, 'is a' ,shpDesc.shapeType
+        print shpDesc.name, 'is a' ,shpDesc.shapeType # x.shp is a Polygon/Point/etc.
         if len(shpDesc.fields) > 6:
-            print shpDesc.name,'has',str(len(shpDesc.fields)),'fields:'
-            for f in shpDesc.fields:
-                print f.name
+            print shpDesc.name,'has',str(len(shpDesc.fields)),'fields:' # x.shp has n fields:
+            for f in shpDesc.fields: # and they are:
+                print f.name 
         if shpDesc.shapeType == 'Polygon':
             print
-            print 'Life is', shpDesc.featureType
+            print 'Life is', shpDesc.featureType # hopefully this polygon feature class is simple... since that's what I want life to be
 
+# walks the path of this script and subdirectories to get all the shapefiles
 def AllShapefiles():
-    result = []
+    result = [] # empty list to add shapefile path string to
     thisDir = os.path.dirname(os.path.abspath(__file__))
     for (new_path, dirs, files) in os.walk(thisDir):
         for f in files:
-            if os.path.splitext(f)[1] == '.shp':
-                shapefile = os.path.join(new_path, f)
+            if os.path.splitext(f)[1] == '.shp': # if it's a shapefile...
+                shapefile = os.path.join(new_path, f) # directory + filename
                 result.append(shapefile)
     return result
 
 
-
+# nerdism
 if __name__ == "__main__":
     main()
