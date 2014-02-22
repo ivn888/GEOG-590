68501b3cea07d3def998523b12a58e5e0b420904
Jason Roebuck <jroebuck@resdat.com>
2014-02-22 15:44:27 -0800

    fixup! use input args, add comments

--- Assignment3/raster_tools.py
+++ Assignment3/raster_tools.py
@@ -1,11 +1,17 @@
 import arcpy
 from arcpy.sa import *
 
+# reprojects input raster to output geodatabase
+# inputs: to/from projection, interpolation method (optional, default: bilinear)
+# returns path to the new raster
 def project_raster(raster, output_geodatabase, to_projection, from_projection, interpolation_method = 'BILINEAR'):
   output_path = output_geodatabase + '/' + raster
   arcpy.ProjectRaster_management(raster, output_path, to_projection, interpolation_method,"#","#","#",from_projection)
   return output_path
 
+# hillshades input DEM raster, or any raster, but you probably won't like the results on something that's not elevation
+# inputs: output geodatabase location
+# returns path to new raster
 def hillshade_raster(raster, output_geodatabase):
   arcpy.CheckOutExtension('Spatial')
   output_path = output_geodatabase + '/' + raster.replace(output_geodatabase, '') + '_hs'
--- Assignment3/test_project_rasters.py
+++ Assignment3/test_project_rasters.py
@@ -1,16 +1,23 @@
 import raster_tools
 import arcpy
 
+# get params
+source_workspace = arcpy.GetParameterAsText(0) # 'D:/Downloads/assignment-3-rasters/smaller2'
+output_geodatabase = arcpy.GetParameterAsText(1) # 'C:/Users/jroebuck/Projects/GEOG-590/Assignment3/Output.gdb'
 
-source_workspace = 'D:/Downloads/assignment-3-rasters/smaller2'#arcpy.GetParameterAsText(0)
-output_geodatabase = 'C:/Users/jroebuck/Projects/GEOG-590/Assignment3/Output.gdb'#arcpy.GetParameterAsText(1)
-
+# set workspace for list rasters method
 arcpy.env.workspace = source_workspace
+# for each raster in workspace
 for raster in arcpy.ListRasters():
+  # save path of output
   projected_raster = raster_tools.project_raster(raster, output_geodatabase,
 	"PROJCS['North_America_Lambert_Conformal_Conic',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Lambert_Conformal_Conic'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-96.0],PARAMETER['Standard_Parallel_1',20.0],PARAMETER['Standard_Parallel_2',60.0],PARAMETER['Latitude_Of_Origin',40.0],UNIT['Meter',1.0]]",
 	"GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]"
   )
   print 'Reprojected raster as', projected_raster
+  # save path of output
   hillshaded_raster = raster_tools.hillshade_raster(projected_raster, output_geodatabase)
   print 'Hillshaded raster as', hillshaded_raster
+
+# if we get here successfully, it works!!
+print 'Success!'
